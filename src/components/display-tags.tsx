75287095bab687d9e2c7a3628e50d5863e54921c
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2024-04-18 03:10:56 +0200

    Adjusting displayTags function. Fixing broken env
    Recently I had to run 'bun init' while setting up tests and happy-dom.
    It altered the package.json file which broke how next works. (it set as
    type: module)

--- src/components/display-tags.tsx
+++ src/components/display-tags.tsx
@@ -2,21 +2,25 @@ import Pill from "@/ui/pill";
 
 export default function displayTags(tags: string[], limit = 3) {
   let tagsRow: JSX.Element[] = [];
+  const tagsSet = new Set(tags);
+  let i = 0;
 
-  for (let i = 0; i < tags.length; i++) {
+  for (const tag of tagsSet) {
     if (i === limit) {
       tagsRow.push(
-        <Pill variant="ghost" key={tags[i] + i}>
-          + {tags.length - limit}
+        <Pill variant="ghost" key={tag + i}>
+          + {tagsSet.size - limit}
         </Pill>,
       );
       break;
     }
+
     tagsRow.push(
-      <Pill variant="primary" key={tags[i] + i}>
-        #{tags[i]}
+      <Pill variant="primary" key={tag + i}>
+        {tag}
       </Pill>,
     );
+    i++;
   }
 
   return tagsRow;
