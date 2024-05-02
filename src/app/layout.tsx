32f19af8e36cde4075ec30ca4c648b849a9d66aa
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2024-05-02 02:31:25 +0200

    Adding Metadata to root layout. Adding helper script
    The script is simply to delete the next cached builds.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import { twJoin } from "tailwind-merge";
-import { josefinSans, outfit, silkScreen } from "@/utils/fonts";
+import { outfit } from "@/utils/fonts";
 
 import "./globals.css";
 import Providers from "@/providers";
 import { Toaster } from "@/components/ui/sonner";
 import Container from "@/components/ui/container";
 import { cn } from "@/lib/utils";
+
+export const metadata = {
+  title: "Articool",
+  description: "Best articles on the web",
+}
 
 export default function RootLayout({
   children,
